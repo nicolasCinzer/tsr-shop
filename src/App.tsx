ebfbc141242b7cd623480b984986c301565aa79c
Nicolas Cinzer <ncinzer@os.aeroterra.com>
2023-06-24 16:45:57 -0500

    Main nodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Header from './components/Header'
+import ProductList from './components/ProductList'
+import Cart from './components/Cart'
+import Footer from './components/Footer'
+import { useState } from 'react'
+
 function App() {
-  return <></>
+  const [viewCart, setViewCart] = useState(false)
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
+    <>
+      <Header
+        viewCart={viewCart}
+        setViewCart={setViewCart}
+      />
+      {pageContent}
+      <Footer viewCart={viewCart} />
+    </>
+  )
+  return content
 }
 
 export default App
