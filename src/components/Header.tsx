de28eb2e7539316d28c119b26bedc3c90b1c9dc9
Nicolas Cinzer <ncinzer@os.aeroterra.com>
2023-06-24 16:46:13 -0500

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,31 @@
-const Header = () => {
-  return <div>Header</div>
+import Nav from './Nav'
+import useCart from '../hooks/useCart'
+
+type HeaderProps = {
+  viewCart: boolean
+  setViewCart: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Header = ({ viewCart, setViewCart }: HeaderProps) => {
+  const { totalItems, totalPrice } = useCart()
+
+  const content = (
+    <header className='header'>
+      <div className='header__title-bar'>
+        <h1>TSR O'Clock</h1>
+        <div className='header__price-box'>
+          <p>Total items: {totalItems}</p>
+          <p>Total price: {totalPrice}</p>
+        </div>
+      </div>
+      <Nav
+        viewCart={viewCart}
+        setViewCart={setViewCart}
+      />
+    </header>
+  )
+
+  return content
 }
 
 export default Header
---
+++ src/components/Nav.tsx
@@ -0,0 +1,18 @@
+type HeaderProps = {
+  viewCart: boolean
+  setViewCart: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Nav = ({ viewCart, setViewCart }: HeaderProps) => {
+  const button = viewCart ? (
+    <button onClick={() => setViewCart(false)}>View Products</button>
+  ) : (
+    <button onClick={() => setViewCart(true)}> View Cart</button>
+  )
+
+  const content = <nav className='nav'>{button}</nav>
+
+  return content
+}
+
+export default Nav
